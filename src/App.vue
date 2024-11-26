2bd196a6a2babee3c567335d714cc27320e0de8f
PixelBoy16 <156637687+PixelBoy16@users.noreply.github.com>
2024-11-26 20:10:00 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1><HelloWorld msg="Hello Kamin" /></h1>
+    <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+    <RouterLink to="/login">Go to Login</RouterLink>
+  </nav>
   <RouterView />
 </template>
 
