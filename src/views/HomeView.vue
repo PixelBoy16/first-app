1b9b77d5567910f967e7e1ba7a3fc38aede9738a
PixelBoy16 <156637687+PixelBoy16@users.noreply.github.com>
2024-11-26 20:04:56 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import FormSignIn from '../components/forms/FormSignIn.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <FormSignIn />
   </main>
 </template>
